f5dec0d6cc9a0b967ff864dd6c7d44c6fd4b40ec
Destiny Aigbe <aigbedestinyic@gmail.com>
2022-10-27 16:14:42 +0100

    Made it look better

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let showInp = true;
 const fetchData = async () => {
   const res = await fetch("https://yts.mx/api/v2/list_movies.json?quality=3D");
   const data = await res.json();
@@ -5,17 +6,83 @@ const fetchData = async () => {
 };
 window.addEventListener("load", async () => {
   let movies = await fetchData();
+  console.log(movies);
   localStorage.setItem("movies", JSON.stringify(movies));
-  const classes = document.getElementsByTagName("a");
-  let arr = Array.from(classes);
-  arr.forEach((clas, i) => {
-    clas.href = `/single.html?id=${movies[i].id}`;
-    clas.innerHTML = `
-    <div class="movie-img"><img src = ${movies[i].background_image} alt="img" /></div>
-    <div class="text">
-        <h3>${movies[i].title}</h3>
-        <p>${movies[i].year}</p>
-    </div>
-    `;
-  });
+  inputEl(movies);
 });
+const run = () => {
+  document.getElementById("input").addEventListener("keyup", (e) => {
+    let movies = JSON.parse(localStorage.getItem("movies"));
+    let regex = new RegExp(e.target.value);
+    let returned = movies.filter((movie) => {
+      return movie.title.toLowerCase().match(regex);
+    });
+    if (e.currentTarget.value) {
+      inputEl(returned);
+    } else {
+      inputEl(movies);
+    }
+    console.log(returned);
+  });
+};
+run();
+
+document.getElementById("filter").addEventListener("click", (ev) => {
+  showInp = !showInp;
+  if (!showInp) {
+    document
+      .getElementById("input")
+      .removeEventListener("keyup", (e) => console.log(e));
+    ev.currentTarget.innerHTML = `
+    Search by Name
+    <img src="./icons/left.svg" height="16px" alt="dropdown" />`;
+    document.getElementById("input").placeholder = "Enter year hear";
+    document.getElementById("input").focus();
+    document.getElementById("input").id = "input-year";
+    document.getElementById("input-year").addEventListener("keyup", (e) => {
+      let movies = JSON.parse(localStorage.getItem("movies"));
+      let returned = movies.filter((movie) => {
+        return (
+          movie.year === parseInt(e.target.value) ||
+          String(movie.year).includes(e.target.value)
+        );
+      });
+      if (e.currentTarget.value) {
+        inputEl(returned);
+      } else {
+        inputEl(movies);
+      }
+    });
+  } else {
+    document.getElementById("input-year").id = "input";
+    document.getElementById("input").focus();
+    document.getElementById("input").placeholder = "Search Name Of Movie";
+    ev.currentTarget.innerHTML = `
+    Search by Year
+    <img src="./icons/dropdown.svg" height="16px" alt="dropdown" />`;
+    run();
+  }
+});
+
+const inputEl = (movies) => {
+  let main = document.getElementById("main");
+  main.innerHTML = "";
+  let i = 0;
+  while (i <= movies.length) {
+    if (!movies[i]) {
+      return;
+    }
+    let a = document.createElement("a");
+    a.href = `/single.html?id=${movies[i].id}`;
+    a.innerHTML = `
+      <div class="movie-img"><img src = ${movies[i].large_cover_image} alt="img" /></div>
+      <div class="text">
+          <h3>${movies[i].title}</h3>
+          <p>${movies[i].year}</p>
+      </div>
+      `;
+    main.appendChild(a);
+    i++;
+  }
+  document.getElementById("main").innerHTML = main.innerHTML;
+};
--- single.js
+++ single.js
@@ -3,6 +3,7 @@ window.addEventListener("load", () => {
   let id = params.get("id");
   if (id) {
     const movies = JSON.parse(localStorage.getItem("movies"));
+    console.log(movies);
     let cont = movies.find((x) => x.id == id);
     render(cont);
   } else {
@@ -12,12 +13,15 @@ window.addEventListener("load", () => {
 
 function render(content) {
   document.getElementById("single").innerHTML = `
-    <img src="${content.background_image}" alt="img" />
+    <img src="${content.large_cover_image}" alt="img" />
       <div class="text">
         <h3>${content.title}</h3>
         <p>${content.year}</p>
         <p>${content.genres.join(" / ")}</p>
         <p>${content.description_full}</p>
+       <a href="${
+         content.torrents[0].url
+       }"><button>Download Torrent File</button></a>
       </div>
     `;
 }
